550edf10b0121bb19c52cb63449bf89f8b0d47ef
balaji38738 <bijjapwar123@gmail.com>
2020-03-22 03:03:05 +0530

    Player options added

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -6,6 +6,29 @@
 echo "-------SNAKE AND LADDER GAME-------"
 
 START=0
+NO_PLAY=0
+SNAKE=1
+LADDER=2
+
+playerPos=$START
+
 echo "Starting position is $START"
 diceValue=$((RANDOM % 6 + 1))
 echo "Dice value is $diceValue"
+
+function playGame() {
+	option=$((RANDOM % 3))
+	case $option in
+		$NO_PLAY)
+			echo "No Play";;
+		$SNAKE)
+			echo "Snake"
+			((playerPos-=$diceValue));;
+		$LADDER)
+			echo "Ladder"
+			((playerPos+=$diceValue));;
+	esac
+	echo "Player position is $playerPos"
+}
+
+playGame
