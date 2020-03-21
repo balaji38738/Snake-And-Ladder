8e6a8245e5241fa3975ba234f1d7ad0c2c97f909
balaji38738 <bijjapwar123@gmail.com>
2020-03-22 03:27:03 +0530

    Played the game till player reaches 100 using while loop

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -9,26 +9,35 @@ START=0
 NO_PLAY=0
 SNAKE=1
 LADDER=2
+WIN=100
 
 playerPos=$START
 
 echo "Starting position is $START"
-diceValue=$((RANDOM % 6 + 1))
-echo "Dice value is $diceValue"
 
 function playGame() {
-	option=$((RANDOM % 3))
-	case $option in
-		$NO_PLAY)
-			echo "No Play";;
-		$SNAKE)
-			echo "Snake"
-			((playerPos-=$diceValue));;
-		$LADDER)
-			echo "Ladder"
-			((playerPos+=$diceValue));;
-	esac
-	echo "Player position is $playerPos"
+	while [ $playerPos -lt $WIN ]
+	do
+		diceValue=$((RANDOM % 6 + 1))
+		echo -e "\nDice value is $diceValue"
+		option=$((RANDOM % 3))
+		case $option in
+			$NO_PLAY)
+				echo "No Play";;
+			$SNAKE)
+				echo "Snake"
+				if [ $((playerPos - diceValue)) -lt 0 ]
+				then
+					playerPos=$START
+				else
+					((playerPos-=$diceValue))
+				fi;;
+			$LADDER)
+				echo "Ladder"
+				((playerPos+=$diceValue));;
+		esac
+		echo "Player position is $playerPos"
+	done
 }
 
 playGame
