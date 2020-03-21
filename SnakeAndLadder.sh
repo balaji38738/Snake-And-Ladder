bd1abe95f3279c5405523019f5f588f65e1b03b0
balaji38738 <bijjapwar123@gmail.com>
2020-03-22 02:29:43 +0530

    Snake and ladder game

---
+++ SnakeAndLadder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+#Problem Statement:- Write a Programme to play snake and ladder game between two players
+#Author:-  Balaji Ijjapwar
+#Date:- 22 March 2020
+
+echo "-------SNAKE AND LADDER GAME-------"
