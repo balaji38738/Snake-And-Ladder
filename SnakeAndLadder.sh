66c5d16ba0536e9658285ecb314db2271c4d626e
balaji38738 <bijjapwar123@gmail.com>
2020-03-22 05:27:49 +0530

    Added function to find winner among two players

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+
 #Problem Statement:- Write a Programme to play snake and ladder game between two players
 #Author:-  Balaji Ijjapwar
 #Date:- 22 March 2020
@@ -11,39 +12,81 @@ SNAKE=1
 LADDER=2
 WIN=100
 
-playerPos=$START
+player1Pos=$START
 
 echo "Starting position is $START"
 
+function playerMove() {
+	playerOption=$1
+	playerPos=$2
+	if [ $playerOption -eq $SNAKE ]
+	then
+		if [ $((playerPos - diceValue)) -lt 0 ]
+		then
+			playerPos=$START
+		else
+			((playerPos-=$diceValue))
+		fi
+	else
+		if [ $((playerPos + diceValue)) -le 100 ]
+		then
+			((playerPos+=$diceValue))
+		fi
+	fi
+	echo $playerPos
+}
+
 function playGame() {
+	player1Pos=$START
+	player2Pos=$START
 	totalDiceRolls=0
-	while [ $playerPos -lt $WIN ]
+	currentPlayer=1
+	while [[ $player1Pos -lt $WIN && $player2Pos -lt $WIN ]]
 	do
+		echo -e "\nCurrent player is player $currentPlayer"
 		diceValue=$((RANDOM % 6 + 1))
 		((totalDiceRolls++))
-		echo -e "\nDice value is $diceValue"
+		echo "Dice value is $diceValue"
 		option=$((RANDOM % 3))
 		case $option in
 			$NO_PLAY)
 				echo "No Play";;
 			$SNAKE)
 				echo "Snake"
-				if [ $((playerPos - diceValue)) -lt 0 ]
-				then
-					playerPos=$START
-				else
-					((playerPos-=$diceValue))
-				fi;;
+				if [ $currentPlayer -eq 1 ]
+            then
+              player1Pos="$( playerMove $SNAKE $player1Pos )"
+            else
+              player2Pos="$( playerMove $SNAKE $player2Pos )"
+            fi;;
 			$LADDER)
 				echo "Ladder"
-				if [ $((playerPos + diceValue)) -le 100 ]
-            then
-					((playerPos+=$diceValue))
+				if [ $currentPlayer -eq 1 ]
+				then
+					player1Pos="$( playerMove $LADDER $player1Pos )"
+				else
+					player2Pos="$( playerMove $LADDER $player1Pos )"
 				fi;;
 		esac
-		echo "Player position is $playerPos"
+			echo "Player 1: $player1Pos	Player 2: $player2Pos"
+		currentPlayer=$((3 - currentPlayer))
 	done
-	echo "The dice was rolled $totalDiceRolls times to reach winning position"
+	echo -e "\nThe dice was rolled $totalDiceRolls times to reach winning position"
 }
 
-playGame
+function findWinner() {
+	if [ $player1Pos -eq 100 ]
+	then
+		echo -e "PLAYER 1 IS THE WINNER\n"
+	else
+		echo -e "PLAYER 2 IS THE WINNER\n"
+   fi
+}
+
+continueGame="y"
+while [ $continueGame == "y" ]
+do
+	playGame
+	findWinner
+	read -p "Do you want to continue playing (y/n) ?: " continueGame
+done
